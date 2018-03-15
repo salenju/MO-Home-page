e1f85156e3c0dffe204448a82f7bb5b4540162d5
ju.dc <judc@bwoil.com>
2018-03-15 09:34:08 +0800

    Home page 引用Bootstrap版本

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(function() {
+    //header
+    $(".search-item-list li").click(function(){
+        var VasselText = $(".search-list .text").text();
+        var PortText = $(this).find("a").text();
+        $(".search-list .text").text(PortText);
+        $(this).find("a").text(VasselText);
+    });
+    $(".welcome-link").click(function(e){
+        e.stopPropagation();
+        $(this).toggleClass("active")
+    })
+    $(document).click(function(){
+        $(".welcome-link").removeClass("active");
+    });
     // banner  补充说明文字tab切换
     $("#b_tab_menu li>a").hover(function() {
         var i = $(this).closest("li").index();
@@ -20,12 +34,11 @@ $(function() {
         // 显示当前hover的icon对应的文字内容
         $("#t_tab_box li").eq(i).addClass("show").siblings().removeClass("show");
         // 显示当前hover的icon对应的下划线标识
-        $("#t_tab_hint li").eq(i).addClass("select").siblings().removeClass("select");
-        console.log(i);
-        },function() {
+        $("#t_tab_hint").addClass("t-tab-hint").css("left",i * 153 + "px");
+    },function() {
         var i = $(this).closest("li").index();
         $("#icon_lists li").eq(i).removeClass("t-active");
         $("#t_tab_box li").eq(i).removeClass("show");
-        $("#t_tab_hint li").eq(i).removeClass("select");
+        $("#t_tab_hint").removeClass("t-tab-hint");
     });
 });
